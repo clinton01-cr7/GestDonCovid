ed1d3aef94ee9d07ff6baf285df0416bde178781
clinton01-cr7 <cmambou06@gmail.com>
2020-11-13 16:49:58 +0100

    init bd gouvernorat and statistic

--- Don/models.py
+++ Don/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Gouvernorat(models.Model):
+    gouvernorat_id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=25)
+
+class Statistique(models.Model):
+    statistiqu_id = models.AutoField(primary_key=True)
+    cas_positif = models.IntegerField()
+    deces = models.IntegerField()
+    guerison = models.IntegerField()
+    cas_actif = models.IntegerField()
+    datest = models.DateField()
+    gouvernorat_FK = models.ForeignKey(Gouvernorat,on_delete=models.CASCADE)
