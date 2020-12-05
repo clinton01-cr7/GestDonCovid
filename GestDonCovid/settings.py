458dd1088a0b4706482c9bd5f8139fe28f6e2391
clinton01-cr7 <cmambou06@gmail.com>
2020-12-05 21:27:13 +0100

    create data base ,interface statistique

--- GestDonCovid/settings.py
+++ GestDonCovid/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Don.apps.DonConfig',
+    'Statistique.apps.StatistiqueConfig',
+    'Admin.apps.AdminConfig',
 ]
 
 MIDDLEWARE = [
